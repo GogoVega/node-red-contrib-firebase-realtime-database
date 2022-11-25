8392ae5fc97dbb020dfc34ddee63580727e5b439
GogoVega <ga.dandele@students.ephec.be>
2022-11-25 10:57:25 +0100

    fix: path verification

--- src/firebase-get.js
+++ src/firebase-get.js
@@ -10,7 +10,7 @@ module.exports = function (RED) {
 		this.on("input", function (msg, send, done) {
 			const path = (config.pathType === "msg" ? msg[config.path] : config.path) || undefined;
 
-			if (path && typeof msg[config.path] !== "string") {
+			if (path && typeof path !== "string") {
 				done("PATH must be a string!");
 				return;
 			}
