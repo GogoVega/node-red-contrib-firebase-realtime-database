3ea6e2248d9a0cb15788f95654c15d67437afdf2
Gauthier Dandele <92022724+GogoVega@users.noreply.github.com>
2023-11-22 11:03:02 +0100

    Enhancement: Add `autocomplete` to the `Path` field (#60)
    The field helps the user to complete the path

--- src/nodes/load-config.ts
+++ src/nodes/load-config.ts
@@ -14,79 +14,37 @@
  * limitations under the License.
  */
 
+import { ConfigNode } from "@gogovega/firebase-config-node/types";
 import { NodeAPI } from "node-red";
-import { existsSync } from "node:fs";
-import { join } from "node:path";
+import { researchConfigNodeExistence } from "../utils/check-config-node";
 
 /**
- * This fake node is used to check the existence of the config-node
- * because Node-RED does not yet allow to define nodes contained in dependencies.
- *
- * See https://github.com/node-red/node-red/issues/569
- *
- * The hack consists of giving to the module loader a link relative to the config-node.
- * If the config node is not found the user must manually install it or resolve path issues.
- *
- * Default path is: `~/.node-red/node_modules/@gogovega/firebase-config-node/build/nodes/firebase-config.js`
+ * This fake node is used:
+ * 1. To check the existence of the config-node See {@link researchConfigNodeExistence}
+ * 2. An endpoint for nodes to request services from - returns options to autcomplete the path field
  */
 module.exports = function (RED: NodeAPI) {
-	const defaultUserDir = "~/.node-red";
-	const moduleName = "@gogovega/firebase-config-node";
-	const path2ConfigNode = "build/nodes/firebase-config.js";
+	researchConfigNodeExistence(RED);
 
-	try {
-		let configNodeInstalledManually: boolean = false;
-		const possiblePath: Array<{ path: string; valide: boolean }> = [];
-		let userDir: string | undefined;
+	RED.httpAdmin.get(
+		"/firebase/rtdb/autocomplete/:id?",
+		RED.auth.needsPermission("load-config.read"),
+		async (req, res) => {
+			const id = req.params.id as string | undefined;
+			const path = req.query.path as string | undefined;
 
-		possiblePath.push({ path: join(__dirname, "../../../../", moduleName, path2ConfigNode), valide: false });
+			if (!id) return res.status(400).send("The config-node ID is missing!");
 
-		if (RED.settings.available() && RED.settings.userDir) {
-			userDir = RED.settings.userDir;
-			possiblePath.push({
-				path: join(RED.settings.userDir, "/node_modules/", moduleName, path2ConfigNode),
-				valide: false,
-			});
+			const node = RED.nodes.getNode(id) as ConfigNode | null;
 
-			const packageFilePath = join(RED.settings.userDir, "package.json");
+			// Like database field not setted or new config-node not yet deployed
+			if (!node) return res.json([]);
 
-			if (packageFilePath) {
-				// eslint-disable-next-line @typescript-eslint/no-var-requires
-				const content = require(packageFilePath);
-				configNodeInstalledManually =
-					typeof content === "object" && content && content.dependencies[moduleName] ? true : false;
-			}
+			const snapshot = await node.rtdb?.get(path);
+			const data = snapshot ? snapshot.val() : {};
+			const options = typeof data === "object" ? Object.keys(data ?? {}) : [];
+
+			return res.json(options);
 		}
-
-		// Check the path existence
-		for (const option of possiblePath) {
-			option.valide = existsSync(option.path);
-		}
-
-		const configNodeFound = possiblePath[0].valide === true;
-
-		if (!configNodeFound && !configNodeInstalledManually) {
-			const [relativePath, pathFromUserDir] = possiblePath;
-			const { path, valide } = pathFromUserDir || {};
-			const secondMessage = `\nThe config-node ${valide ? "is" : "is not"} located in\n  ${path}\n`;
-			const userDirNotDefault = relativePath.path !== path && path;
-			const installFromNPM = `${moduleName}@latest --omit=dev`;
-			const installFromFile = `file:${path?.split(path2ConfigNode)[0]}`;
-			const installLink = valide ? installFromFile : installFromNPM;
-
-			console.error(`
-Firebase ERR!  Unable to find the config-node!
-The config-node is not located in
-  ${relativePath.path}
-${userDirNotDefault ? secondMessage : ""}
-  To resolve this problem, please run:
-    cd ${userDir || defaultUserDir}
-    npm i ${installLink} --save
-${userDirNotDefault ? "\n  Or resolve the path in the 'package.json' file\n" : ""}
-Read more about this problem at https://github.com/GogoVega/node-red-contrib-firebase-realtime-database/discussions/50
-`);
-		}
-	} catch (error) {
-		console.error("\nFirebase ERR! An error occurred while searching for the config-node.\n");
-	}
+	);
 };
