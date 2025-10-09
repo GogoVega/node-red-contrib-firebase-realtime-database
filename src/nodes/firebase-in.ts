493664176964ac3a4dfb39bc51510bf536b1a914
GogoVega <92022724+GogoVega@users.noreply.github.com>
2025-10-09 15:33:35 +0200

    Remove the `load-config` node

--- src/nodes/firebase-in.ts
+++ src/nodes/firebase-in.ts
@@ -15,10 +15,52 @@
  */
 
 import { NodeAPI } from "node-red";
+import { ConfigNode } from "@gogovega/firebase-config-node/types";
 import { FirebaseIn } from "../lib/firebase-node";
 import { FirebaseInConfig, FirebaseInNode } from "../lib/types";
 
 module.exports = function (RED: NodeAPI) {
+	// Get autocomplete options for all Firebase nodes
+	RED.httpAdmin.get(
+		"/firebase/rtdb/autocomplete/:id?",
+		RED.auth.needsPermission("firebase-in.write"),
+		async (req, res) => {
+			const id = req.params.id as string | undefined;
+			const path = req.query.path as string | undefined;
+
+			if (!id) {
+				res.status(400).send("The config-node ID is missing!");
+				return;
+			}
+
+			const node = RED.nodes.getNode(id) as ConfigNode | null;
+
+			// Like database field not setted or new config-node not yet deployed
+			if (!node) {
+				res.json([]);
+				return;
+			}
+
+			try {
+				// May fail if permission is denied
+				const snapshot = await node.rtdb?.get(decodeURI(path || ""));
+				const data = snapshot ? snapshot.val() : {};
+				const options = typeof data === "object" ? Object.keys(data ?? {}) : [];
+
+				res.json(options);
+			} catch (error) {
+				const msg = error instanceof Error ? error.message : String(error);
+
+				if (/(permission_denied|Permission denied)/.test(msg)) {
+					res.json([]);
+					return;
+				}
+
+				res.status(500).send({ message: String(error) });
+			}
+		}
+	);
+
 	function FirebaseInNode(this: FirebaseInNode, config: FirebaseInConfig) {
 		RED.nodes.createNode(this, config);
 
